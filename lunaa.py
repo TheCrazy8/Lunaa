80dd95ef953e9e94f7eb3a05ca8d7ce672ddd326
TheCrazy8 <bennettbashford@gmail.com>
2025-10-20 13:24:57 -0400

    dd

--- lunaa.py
+++ lunaa.py
@@ -5,24 +5,7 @@ import random
 import datetime
 import tkinter as tk
 import tkinter.ttk as ttk
-import ollama
+import tensorflow
 import sv-ttk
 import numpy
-from ollama import chat
 
-
-ollama.pull('gemma3')
-
-def chat():
-    content=input("Messagetext here: ")
-    stream = chat(
-    model='gemma3',
-    messages=[{'role': 'user', 'content': content}],
-    stream=True,
-    )
-
-    for chunk in stream:
-        print(chunk['message']['content'], end='', flush=True)
-
-def vision():
-    
