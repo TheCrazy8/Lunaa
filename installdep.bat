5b826b6bd791fb90ae532c567893701aa232b0d1
Scarlett/Eight <bennettbashford@gmail.com>
2025-11-20 16:29:05 -0500

    Add additional packages to installdep.bat

--- installdep.bat
+++ installdep.bat
@@ -1,1 +1,1 @@
-pip install transformers numpy pandas datasets torch torchvision tqdm
+pip install transformers numpy pandas datasets torch torchvision tqdm scrapy spacy nltk langchain instructor scikit-llm ollama mcp-server-git 
