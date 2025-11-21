7f92724781b0ede52d094bd47ff92b023599f8ee
Scarlett/Eight <bennettbashford@gmail.com>
2025-11-21 11:05:41 -0500

    Add 'ssv_ttk' to pip install command

--- installdep.bat
+++ installdep.bat
@@ -1,1 +1,1 @@
-pip install transformers numpy pandas datasets torch torchvision tqdm scrapy spacy nltk langchain instructor scikit-llm ollama mcp-server-git 
+pip install transformers numpy pandas datasets torch torchvision tqdm scrapy spacy nltk langchain instructor scikit-llm ollama mcp-server-git ssv_ttk
